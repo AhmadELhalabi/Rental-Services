ba19f65b0ebad53b3319103c8c1c496b1cd5d9e7
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-02-26 20:38:41 +0300

    def init

---
+++ RentServices.py
@@ -0,0 +1,9 @@
+class Vehical:
+# Initializing the vehicle with its type, brand, model, year, rental price, and special attribute (seats or engine capacity)
+    def _init_(self, VehicalType, brand, model, year, RentPricePerDay, SpecialAttribute ):
+       self.VehicalType = VehicalType
+       self.brand = brand 
+       self.model = model
+       self.year = year
+       self.RentPricePerDay = RentPricePerDay 
+       self.SpecialAttribute = SpecialAttribute
